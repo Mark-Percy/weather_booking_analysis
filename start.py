cc3c763689b8c720d970f11f9a04255e1af4c5ab
Mark <mark.percy98@outlook.com>
2020-08-19 14:40:58 +0100

    added pandas for dates in in the data set ability to add dates that dont exist

--- start.py
+++ start.py
@@ -9,12 +9,11 @@ from datetime import date,timedelta
 from random import randint
 
 dates = pd.read_csv("data/dates.txt")
-print(dates)
 
 
 class Day:
     def __init__(self, date, weather, num_of_bookings):
-        self.date = date.strftime("%d/%m/%Y")
+        self.date = date
         self.weather = weather
         self.num_of_bookings = num_of_bookings
 
@@ -22,11 +21,19 @@ class Day:
         return f"On {self.date}\
 \nthere are {self.num_of_bookings} left\n\
 forcast: {self.weather.weather}, Temp: {self.weather.min_temp} - {self.weather.max_temp}"
-#pd.DataFrame(columns=["ID","date"])
+
+
     def add_to_data(self, date_data):
-        if pd.to_datetime(self.date) not in date_data:
-            pass
-        day = pd.DataFrame({})
+        i_d = 0
+        if pd.Timestamp(self.date) not in pd.to_datetime(date_data["date"].values):
+            if not date_data.empty:
+                i_d = [date_data.iloc[-1]["ID"]+1]
+            else:
+                i_d = 1
+            
+            day = pd.DataFrame(data= {"ID": i_d ,"date": [self.date]})
+            date_data = date_data.append(day)
+        return {"dates" : date_data}
 
 
 weather_soup = GetSite("https://www.google.com/search?q=aintree+weather").get_soup()
@@ -40,9 +47,9 @@ for i in range(6):
     #num = len(GetSite("https://aintree.intelligentgolf.co.uk/visitorbooking/?date=" + the_date.strftime("%d-%m-%Y")).get_soup().find_all("td", class_ = "bookable:4"))
     num = randint(1,30)
     weather = Weather(weather_soup,the_date)
-    days.append(Day(the_date, weather, num))
+    day = (Day(the_date, weather, num))
+    dates = day.add_to_data(dates)
     the_date += delta
 
-for day in days:
-    print(day)
-    print()
+print(dates)
+dates.to_csv("data/dates.txt", index=False)
